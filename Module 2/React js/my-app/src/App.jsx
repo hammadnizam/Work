fbd0f2017cb8f2f13e885720e0ca5d766d998085
hammad <hammadnizam65>
2024-08-27 11:39:17 -0700

    new

---
+++ Module 2/React js/my-app/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+
+  // const vegetable = [
+  //   "tomato" , "patato" ,"bringle" , "matter" , "gobbi" 
+  // ]
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <ol>{
+          vegetable.map((data)=>{
+          return <li>{data}</li>
+          })
+        }
+        </ol> */}
+        
+<div>
+  <h1>Stylo Bhai</h1>
+</div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
